da49b70b0ead1ee9dd1991a9d77e5be1ddbc92fb
martin baumgaertner <92078725+martinbaumg@users.noreply.github.com>
2022-11-17 12:53:50 +0100

    optimisation messagerie

--- Socket/client.py
+++ Socket/client.py
@@ -7,25 +7,17 @@ client_socket = socket.socket()
 client_socket.connect(('127.0.0.1', 10000))
 print("Connexion établie...")
 
-menu = input("Menu général, que voulez-vous faire ?\n1. Envoyer un message\n2. Quitter\n")
 
-if menu == "1" :
-    while message != "quit":
-        message = input("que voulez-vous envoyer : ")
-        print("Message envoyé... en attente d'une réponse")
-        client_socket.send(message.encode())
-        data = client_socket.recv(1024).decode()
-        print(f"Message reçu : {data}")
+while message != "q":
+    message = input("vous (q pour quitter) : ")
+    client_socket.send(message.encode())
+    print("Message envoyé... en attente d'une réponse")
+    data = client_socket.recv(1024).decode()
+    print(f"serveur : {data}")
 
-code = input("voulez vous quitter ? (y/n) : ")
 
-if code == "y" or menu =="2" or message == "quit":
+if message == "q":
     client_socket.close()
 
-# else :
-#     message = input("que voulez-vous envoyer : ")
-#     client_socket.send(message.encode())
-#     data = client_socket.recv(1024).decode()
-#     print(f"Message reçu : {data}")
 
 
--- Socket/serveur.py
+++ Socket/serveur.py
@@ -4,32 +4,22 @@ import socket
 message = "rien"
 
 server_socket = socket.socket()
-print("Socket crée, connexion en cours...")
+print("Socket crée, en attente du client...")
 server_socket.bind(('127.0.0.1', 10000))
 server_socket.listen(1)
 
+
+
+conn, address = server_socket.accept()
 print("Connexion établie...")
+while message != "q":
+    reply = input("vous (q pour quitter) : ")
+    print("Message envoyé... en attente d'une réponse")
+    data = conn.recv(1024).decode()
+    conn.send(reply.encode())
+    print(f"client : {data}")
 
-menu = input("Menu général, que voulez-vous faire ?\n1. Envoyer un message\n2. Quitter\n")
 
-if menu == "1":
-     conn, address = server_socket.accept()
-     while message != "quit":
-        reply = input("que voulez-vous envoyer : ")
-        print("Message envoyé... en attente d'une réponse")
-       
-        data = conn.recv(1024).decode()
-        conn.send(reply.encode())
-        print(f"Message reçu : {data}")
-
-code = input("voulez vous quitter ? (y/n) : ")
-
-if code == "y" or menu =="2" or message == "quit":
+if message == "q":
     conn.close()
     server_socket.close()
-
-# else : 
-#     reply = input("que voulez-vous envoyer : ")
-#     conn.send(reply.encode())
-#     data = conn.recv(1024).decode()
-#     print(f"Message reçu : {data}")
