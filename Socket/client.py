6b5ff9e20668a0f0432e3975e346bc2a7a1b42b2
martin baumgaertner <92078725+martinbaumg@users.noreply.github.com>
2022-11-18 19:36:43 +0100

    test

--- Socket/client.py
+++ Socket/client.py
@@ -19,3 +19,25 @@ while message != "q":
 if message == "q":
     client_socket.close()
 
+# import socket
+
+# # Create one client socket
+# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client.connect(('127.0.0.1', 10000))
+# print(f"Client connected to server")
+# client.send("Hello Server".encode())
+# data = client.recv(1024).decode()
+# print(data)
+
+# while True:
+#     message = input("Enter your message: ") # Possible to send message to the server
+#     client.send(message.encode())
+#     if message == "exit": # Close the server when client send "exit" message
+#         client.close()
+#         break
+#     elif message == "close": # Close the client connection but not down the server if client send "close"
+#         print ("Client disconnected")
+#         break
+
+# if __name__ == '__main__':
+#     pass
--- Socket/serveur.py
+++ Socket/serveur.py
@@ -22,3 +22,40 @@ while message != "q":
 if message == "q":
     conn.close()
     server_socket.close()
+# import socket
+
+# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server.bind(('127.0.0.1', 10000))
+# server.listen(5)
+# print("Server is listening...")
+# clientconnected = False
+# print ("Waiting for client...")
+
+# # Wait for a client to connect
+# while clientconnected == False:
+#     client, address = server.accept()
+#     print("Client connected: ", address)
+#     clientconnected = True
+#     client.send ("Hello Client".encode())
+
+# # Receive data from the client when client connected
+# while clientconnected == True:
+#     data = client.recv(1024).decode()
+#     print(f"Client {address} send: {data}")
+#     if data == "exit": # Close the connection when client send "exit" message
+#         server.close()
+#         print (f"Server closed by {address}")
+#         break
+#     elif data == "close":  # Close the client connection but not the server if client send "close"
+#         print("Client disconnected")
+#         client.close()
+#         clientconnected = False 
+#         print ("Waiting for client...")
+#         while clientconnected == False: # Wait for a new client to connect
+#             client, address = server.accept()
+#             print("Client connected: ", address)
+#             clientconnected = True
+#             client.send ("Hello Client".encode())
+
+# if __name__ == '__main__':
+#     pass
