3e43f2088e994aaf6ee857fac4c78d6e2ec33481
martin baumgaertner <92078725+martinbaumg@users.noreply.github.com>
2022-11-25 15:43:38 +0100

    bouton aide

--- Interface-graphique/conversion.py
+++ Interface-graphique/conversion.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QToolTip, QMessageBox, QMainWindow, QGridLayout, QLabel, QLineEdit
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QToolTip, QMessageBox, QMainWindow, QGridLayout, QLabel, QLineEdit, QComboBox
 
 # Une fenêtre qui convertit des degrés en kelvin
 
@@ -14,11 +14,27 @@ class Window(QWidget):
         self.label = QLabel("Degrés : ")
         self.label2 = QLabel("°C")
         self.line_edit = QLineEdit()
-        self.button = QPushButton("Valider")
+        self.button = QPushButton("Convertir")
         self.button.clicked.connect(self.valider)
         self.text = QLabel("")
         self.quit = QPushButton("Quitter")
         self.quit.clicked.connect(self.__actionQuitter)
+        self.button2 = QPushButton("Aide")
+        # mettre un menu déroulant avec degré et kelvin
+        self.menu = QComboBox()
+        self.menu.addItem("°C -> K")
+        self.menu.addItem("K -> °C")
+
+        # en fonction de ce qu'on a choisi, la conversion change
+        self.menu.currentIndexChanged.connect(self.validation)
+
+        # une fenêtre apparaît quand on clique sur le bouton aide
+        self.button2.clicked.connect(self.aide)
+
+
+
+
+        
 
         layout = QGridLayout()
         layout.addWidget(self.label, 0, 0)
@@ -27,13 +43,28 @@ class Window(QWidget):
         layout.addWidget(self.text, 2, 0, 1, 2)
         layout.addWidget(self.quit, 3, 0, 1, 2)
         layout.addWidget(self.label2, 0, 2)
+        layout.addWidget(self.menu, 0, 3)
+        layout.addWidget(self.button2, 1, 3)    
+
 
         self.setLayout(layout)
+
+    def aide(self):
+        QMessageBox.information(self, "Aide", "Entrez un nombre de degrés et cliquez sur convertir")
+        
+
+    def validation(self):
+        degres = self.line_edit.text()
+        kelvin = float(degres) - 273.15
+        self.text.setText(f"{kelvin} °C")
+        self.label2.setText("K")
 
     def valider(self):
         degres = self.line_edit.text()
         kelvin = float(degres) + 273.15
         self.text.setText(f"{kelvin} K")
+        self.label2.setText("°C")
+
     def __actionQuitter(self):
         QCoreApplication.exit(0)
 
