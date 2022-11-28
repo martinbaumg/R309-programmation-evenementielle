6a381eb4a04ff81670217e172488e0db0d7775c4
martin baumgaertner <92078725+martinbaumg@users.noreply.github.com>
2022-11-28 08:57:02 +0100

    ajouts mineurs

--- Socket/client.py
+++ Socket/client.py
@@ -1,66 +1,23 @@
-# import socket
-
-# message = "rien"
-
-
-# client_socket = socket.socket()
-# client_socket.connect(('127.0.0.1', 10000))
-# print("Connexion établie...")
-
-
-# while message != "q":
-#     message = input("vous (q pour quitter) : ")
-#     client_socket.send(message.encode())
-#     print("Message envoyé... en attente d'une réponse")
-#     data = client_socket.recv(1024).decode()
-#     print(f"serveur : {data}")
-
-
-# if message == "q":
-#     client_socket.close()
-
 import socket
-import select
-import errno
-import time
 
-HEADER_LENGTH = 10
+message = "rien"
 
-IP = "127.0.0.1"
-PORT = 10000
-data = "rien"
-my_username = input("Nom: ")
+
 client_socket = socket.socket()
-client_socket.connect((IP, PORT))
-client_socket.setblocking(False)
-username = my_username.encode('utf-8')
-username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
-client_socket.send(username_header + username)
-while True:
-    message = input(f'{my_username} > ')
-    # Pour avoir un lecteur de message mettre en commentaire la ligne du dessus et décommenter celle du dessous
-    # message = ""
-    if message:
-        message = message.encode('utf-8')
-        message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
-        client_socket.send(message_header + message)
-    try:
-        while True:
-            username_header = client_socket.recv(HEADER_LENGTH)
-            if not len(username_header):
-                print('Connexion fermée par le serveur')
-                sys.exit()
-            username_length = int(username_header.decode('utf-8').strip())
-            username = client_socket.recv(username_length).decode('utf-8')
-            message_header = client_socket.recv(HEADER_LENGTH)
-            message_length = int(message_header.decode('utf-8').strip())
-            message = client_socket.recv(message_length).decode('utf-8')
-            print(f'{username} > {message}')
-    except IOError as e:
-        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-            print('Erreur: {}'.format(str(e)))
-            sys.exit()
-        continue
-    except Exception as e:
-        print('Erreur: '.format(str(e)))
-        sys.exit()
+client_socket.connect(('127.0.0.1', 10000))
+print("Connexion établie...")
+
+
+while message != "bye":
+    message = input("vous (bye pour quitter) : ")
+    client_socket.send(message.encode())
+    print("Message envoyé... en attente d'une réponse")
+    data = client_socket.recv(1024).decode()
+    print(f"serveur : {data}")
+
+
+if message == "bye":
+    client_socket.close()
+
+
+
